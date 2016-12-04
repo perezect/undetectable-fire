9cc314dbec9296807620955ab9ab0549d8cb85ce
williumchen <wichen@g.hmc.edu>
2016-12-03 17:09:49 -0800

    background subtraction

--- detect.py
+++ detect.py
@@ -1,5 +1,23 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+
+fire_cap = cv2.VideoCapture('src/fire.avi')
+fgbg = cv2.createBackgroundSubtractorMOG2()
+
+while True:
+    ret, frame = fire_cap.read()
+
+    fgmask = fgbg.apply(frame)
  
- 
+    cv2.imshow('fgmask',frame)
+    cv2.imshow('frame',fgmask)
+
+    
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+    
+
+fire_cap.release()
+cv2.destroyAllWindows()
