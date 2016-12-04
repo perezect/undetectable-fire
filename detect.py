da91d2f1b45257a1a7a5f651daad0e7dc658e62e
Felis Perez <felisperez@Princess-Celestia.local>
2016-12-04 01:06:36 -0800

    Commented code for smoke detection? Not sure if the thresholding needed work

--- detect.py
+++ detect.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 
 cv2.ocl.setUseOpenCL(False)
-cap = cv2.VideoCapture('src/fire.mp4')
+cap = cv2.VideoCapture('src/roman_candle.mp4')
 fgbg = cv2.createBackgroundSubtractorMOG2()
 
 while(1):
@@ -10,22 +10,39 @@ while(1):
 
     # Change colorspace from RGB to HSV (hue saturation value)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    
+
     # Threshold for fire
     sensitivity = 100
     lower_fire = np.array([0, 0, 255-sensitivity], dtype=np.uint8)
     upper_fire = np.array([60, sensitivity, 255], dtype=np.uint8)
-    
+
     mask = cv2.inRange(hsv, lower_fire, upper_fire)
     res = cv2.bitwise_and(frame, frame, mask= mask)
+
+    # Threshold for smoke
+    lower_smoke = np.array([200,200,200], dtype=np.uint8)
+    upper_smoke = np.array([255,255,255], dtype=np.uint8)
+
+    smask = cv2.inRange(hsv, lower_smoke, upper_smoke)
 
     # Background subtraction
     fgmask = fgbg.apply(res)
 
+
     cv2.imshow('frame', frame)
-    cv2.resizeWindow('frame', 600,600)
     cv2.imshow('fgmask', fgmask)
-    cv2.resizeWindow('res', 600,600)
+
+    # Threshold for smoke
+    # Likely needs tinkering of the hue values
+    # lower_smoke = np.array([200,200,200], dtype=np.uint8)
+    # upper_smoke = np.array([255,255,255], dtype=np.uint8)
+
+    # smask = cv2.inRange(hsv, lower_smoke, upper_smoke)
+    # smres = cv2.bitwise_and(frame, frame, mask= smask)
+
+    # smmask = fgbg.apply(smres)
+
+    # cv2.imshow('smmask', smmask)
 
     k = cv2.waitKey(30) & 0xff
     if k == 27:
